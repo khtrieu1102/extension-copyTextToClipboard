715fc3e53645287598deae5ac9ddb635ca94548e
KhacTrieu <khtrieu1102@gmail.com>
2019-10-30 18:40:59 +0700

    add more buttons and delete 'add button' function

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,57 +1,56 @@
-document.addEventListener('DOMContentLoaded', LoadButton);
+document.addEventListener("DOMContentLoaded", LoadButton);
 
 function LoadButton() {
-    var x = document.getElementsByClassName("btnClick");
-    for (var i = 0; i < x.length; i++) {
-        x[i].addEventListener("click", CopyMe);
-        x[i].index = i;
-    }
+	var x = document.getElementsByClassName("btnClick");
+	for (var i = 0; i < x.length; i++) {
+		x[i].addEventListener("click", CopyMe);
+		x[i].index = i;
+	}
 
-    var btnAddjs = document.getElementsByClassName("btnAdd");
-    btnAddjs[0].addEventListener("click", function() {addButton("text")});
+	var btnAddjs = document.getElementsByClassName("btnAdd");
+	btnAddjs[0].addEventListener("click", function() {
+		addButton("text");
+	});
 }
 
 function CopyMe(evt) {
-    var indexx = evt.target.index; 
-    var copyText = document.getElementsByClassName("content")[indexx];
-    var showText = document.getElementById("demo");
-    copyText.select();
-    document.execCommand("copy");
-    console.log("Hello mather");
-    showText.textContent = "Copied button: " + indexx;
+	var indexx = evt.target.index;
+	var copyText = document.getElementsByClassName("content")[indexx];
+	var showText = document.getElementById("demo");
+	copyText.select();
+	document.execCommand("copy");
+	showText.textContent = "Copied button: " + indexx;
 }
 
 function addButton(type) {
-    //Create an input type dynamically.   
-    var obj = { 
-        table:[]
-    };
-    var InputName = document.getElementById("addName");
-    var InputContent = document.getElementById("addContent");
+	//Create an input type dynamically.
+	var obj = {
+		table: []
+	};
+	var InputName = document.getElementById("addName");
+	var InputContent = document.getElementById("addContent");
 
-    // Check if both input are empty
-    if (InputName.value.trim() != "" && InputContent.value.trim() != "") {
-    
-        // Add a button
-        var buttonElement = document.createElement("input"); 
-        buttonElement.type = "button"; 
-        buttonElement.className = "btnClick";
-        buttonElement.value = InputName.value;
-        var foo = document.getElementById("content-container");
-        foo.appendChild(buttonElement);
+	// Check if both input are empty
+	if (InputName.value.trim() != "" && InputContent.value.trim() != "") {
+		// Add a button
+		var buttonElement = document.createElement("input");
+		buttonElement.type = "button";
+		buttonElement.className = "btnClick";
+		buttonElement.value = InputName.value;
+		var foo = document.getElementById("content-container");
+		foo.appendChild(buttonElement);
 
-        // Load a textarea to copy
-        var textareaElement = document.createElement("textarea");
-        textareaElement.type = "text"; 
-        textareaElement.className = "content";
-        textareaElement.value = InputContent.value;
-        var foo = document.getElementById("content-container");
-        
-        foo.appendChild(textareaElement);
-        
-        LoadButton();// Load again
-    }
-    else {
-        alert("Please fill all blanks!");
-    }
+		// Load a textarea to copy
+		var textareaElement = document.createElement("textarea");
+		textareaElement.type = "text";
+		textareaElement.className = "content";
+		textareaElement.value = InputContent.value;
+		var foo = document.getElementById("content-container");
+
+		foo.appendChild(textareaElement);
+
+		LoadButton(); // Load again
+	} else {
+		alert("Please fill all blanks!");
+	}
 }
